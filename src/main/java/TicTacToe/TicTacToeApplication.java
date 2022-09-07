ab163ca4630f5b15dfab23c165a10b2fcc7b8f85
PawelHarasiuk <pharasiuk01@gmail.com>
2022-09-07 17:57:13 +0200

    Start Project

---
+++ src/main/java/TicTacToe/TicTacToeApplication.java
@@ -0,0 +1,16 @@
+package TicTacToe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeApplication.class, args);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+}
