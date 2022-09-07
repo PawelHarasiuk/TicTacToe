9b5033e4b3266cf2137a4e15ec94ea8731dd68d4
PawelHarasiuk <pharasiuk01@gmail.com>
2022-09-07 20:20:36 +0200

    Created layout

--- src/main/java/TicTacToe/GameController.java
+++ src/main/java/TicTacToe/GameController.java
@@ -1,11 +1,9 @@
 package TicTacToe;
 
 import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-@Route("")
 @PageTitle("TicTacToe")
 @RestController
 public class GameController {
--- src/main/java/TicTacToe/GameService.java
+++ src/main/java/TicTacToe/GameService.java
@@ -1,27 +1,66 @@
 package TicTacToe;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
 import org.springframework.stereotype.Service;
 
+/*
+ to do:
+ - delete GameController?
+ - add logic
+ - move game to the centre
+ */
+
+@Route("")
 @Service
 public class GameService extends VerticalLayout {
-    private Button
-            b0 = new Button(),
-            b1 = new Button(),
-            b2 = new Button(),
-            b3 = new Button(),
-            b4 = new Button(),
-            b5 = new Button(),
-            b6 = new Button(),
-            b7 = new Button(),
-            b8 = new Button(),
-            bReset = new Button();
-    private Button[] buttons;
+    private H1 text;
+    private Button bReset;
+    private final Button[] buttons;
+    private final HorizontalLayout l1 = new HorizontalLayout();
+    private final HorizontalLayout l2 = new HorizontalLayout();
+    private final HorizontalLayout l3 = new HorizontalLayout();
 
     public GameService(){
-        buttons = new Button[]{
-                b0,b1,b3,b4,b5,b6,b7,b8
-        };
+        buttons = new Button[9];
+        addButtons();
+    }
+
+    public void createButtons(){
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new Button();
+            buttons[i].setHeight("200px");
+            buttons[i].setWidth("200px");
+        }
+        bReset = new Button("Reset");
+        bReset.setHeight("200px");
+        bReset.setWidth("630px");
+    }
+
+    public void addButtons(){
+        createButtons();
+        for (int i = 0; i < buttons.length; i++) {
+            if (i<3){
+                l1.add(buttons[i]);
+            }else if (i>5){
+                l3.add(buttons[i]);
+            } else {
+                l2.add(buttons[i]);
+            }
+        }
+        add(l1,l2,l3,bReset);
+    }
+
+    public void reset(){
+        for (Button b : buttons) {
+
+        }
+    }
+
+    public void addText(){
+
     }
 }
