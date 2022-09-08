528f6a0a9cbe1efca46f71b8dd8bf1e3da9a6d7a
PawelHarasiuk <pharasiuk01@gmail.com>
2022-09-08 14:40:16 +0200

    Created layout

--- src/main/java/TicTacToe/GameService.java
+++ src/main/java/TicTacToe/GameService.java
@@ -10,14 +10,15 @@ import org.springframework.stereotype.Service;
 /*
  to do:
  - delete GameController?
- - add logic
  - move game to the centre
  */
 
 @Route("")
 @Service
 public class GameService extends VerticalLayout {
-    private H1 text;
+
+    private boolean xTurn;
+    private final H1 text = new H1("TicTacToe");
     private Button bReset;
     private final Button[] buttons;
     private final HorizontalLayout l1 = new HorizontalLayout();
@@ -26,7 +27,7 @@ public class GameService extends VerticalLayout {
 
     public GameService(){
         buttons = new Button[9];
-        addButtons();
+        play();
     }
 
     public void createButtons(){
@@ -36,12 +37,11 @@ public class GameService extends VerticalLayout {
             buttons[i].setWidth("200px");
         }
         bReset = new Button("Reset");
-        bReset.setHeight("200px");
+        bReset.setHeight("100px");
         bReset.setWidth("630px");
     }
 
     public void addButtons(){
-        createButtons();
         for (int i = 0; i < buttons.length; i++) {
             if (i<3){
                 l1.add(buttons[i]);
@@ -51,16 +51,57 @@ public class GameService extends VerticalLayout {
                 l2.add(buttons[i]);
             }
         }
-        add(l1,l2,l3,bReset);
+        add(text,l1,l2,l3,bReset);
     }
 
-    public void reset(){
+    public void setButtonsAction(){
         for (Button b : buttons) {
+            b.addClickListener(buttonClickEvent -> {
+                b.setEnabled(false);
+                b.setText(xTurn?"X":"O");
+                checkIfGameIsOver();
+                xTurn=!xTurn;
+            });
+        }
 
+        bReset.addClickListener(buttonClickEvent -> {
+            for (Button b : buttons) {
+                b.setText("");
+                b.setEnabled(true);
+            }
+            text.setText("aaa");
+        });
+    }
+
+    public void checkIfGameIsOver(){
+        for (int a = 0; a < 8; a++) {
+            String line = switch (a) {
+                case 0 -> buttons[0].getText() + buttons[1].getText() + buttons[2].getText();
+                case 1 -> buttons[3].getText() + buttons[4].getText() + buttons[5].getText();
+                case 2 -> buttons[6].getText() + buttons[7].getText() + buttons[8].getText();
+                case 3 -> buttons[0].getText() + buttons[4].getText() + buttons[8].getText();
+                case 4 -> buttons[2].getText() + buttons[4].getText() + buttons[6].getText();
+                case 5 -> buttons[0].getText() + buttons[3].getText() + buttons[6].getText();
+                case 6 -> buttons[1].getText() + buttons[4].getText() + buttons[7].getText();
+                case 7 -> buttons[2].getText() + buttons[5].getText() + buttons[8].getText();
+                default -> null;
+            };
+
+            //X winner
+            if (line.equals("XXX")) {
+                text.setText("X won!");
+            }
+
+            //O winner
+            else if (line.equals("OOO")) {
+                text.setText("O won!");
+            }
         }
     }
 
-    public void addText(){
-
+    public void play(){
+        createButtons();
+        addButtons();
+        setButtonsAction();
     }
 }
